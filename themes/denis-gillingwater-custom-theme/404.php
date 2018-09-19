3224ab2fc5ce340501395256bdb7dc510f24d844
madnosam <mason_drury@hotmail.com>
2018-09-19 14:40:47 -0700

    404 round 3

--- themes/denis-gillingwater-custom-theme/404.php
+++ themes/denis-gillingwater-custom-theme/404.php
@@ -1,8 +1,64 @@
+<!doctype html>
 
-<a id="logo" href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a>
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+
+		<?php // mobile meta (hooray!) ?>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<?php // or, set /favicon.ico for IE10 win ?>
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<?php // wordpress head functions ?>
+		<?php wp_head(); ?>
+		<?php // end of wordpress head ?>
+
+		<?php // drop Google Analytics Here ?>
+		<?php // end analytics ?>
+
+	</head>
+
+	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+
+		<div id="container">
+
+			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+
+				<div id="inner-header" class="cf">
+
+					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
+					<a id="logo" href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a>
+					<?php // if you'd like to use the site description you can un-comment it below ?>
+					<?php // bloginfo('description'); ?>
+
+				</div>
+
+			</header>
 			<div id="content">
 
-				<div id="inner-content" class="wrap cf">
+				<div id="inner-content" class="cf">
 
 					<main id="main" class="m-all t-3of3 d-7of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
@@ -17,7 +73,7 @@
 							<section class="entry-content">
 
 								<p><?php _e( 'The article you were looking for was not found.', 'bonestheme' ); ?></p>
-								<p href='<?php echo home_url(); ?>'> Escape </p>
+								<a href='<?php echo home_url(); ?>'> Escape </a>
 
 							</section>
 
